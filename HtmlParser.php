0baf2850abb1b3f5849defbe7136dd4dab5aff8d
nofikoff <ruslan.novikov@gmail.com>
2016-09-27 11:59:04 +0300

    Add files via upload

--- HtmlParser.php
+++ HtmlParser.php
@@ -6,6 +6,7 @@
 namespace nofikoff\parsermachine;
 
 use DotPack\PhpBoilerPipe;
+
 //use Yii;
 
 class HtmlParser
@@ -26,50 +27,99 @@ class HtmlParser
 
 
     //!!! кстати можно старым добрым СИПЛОДОМОМО echo file_get_html('http://www.google.com/')->plaintext;
+    function __construct($content)
+    {
+
+        if ($this->detect_encoding($content) == 'windows-1251') {
+            $this->content = mb_convert_encoding($content, 'utf-8', 'windows-1251');
+        } else {
+            $this->content = $content;
+        }
+
+
+    }
+
 
     public function html_pars()
     {
-        // грязная очистка
-        $this->content = preg_replace('/(<script[^>]*>.*?<\/script>)/si', '', $this->content);
 
 
-        $result['error_descr'] = '';
+        $result['description_parser'] = '';
+        $result['status'] = '';
         $result['max_paragraf'] = 0;
         $result['mdl_paragraf'] = 0;
+
+
+
+        $result['lang'] = '';
+        $result['title'] = '';
+        $result['description_parser'] = '';
+        $result['h1'] = '';
+
+
         //
         if (!trim($this->content)) {
             $result['status'] = 'parsered_notext';
             $result['error'] = 1;
-            $result['error_descr'] = 'zerro size page AI';
-            $result['content'] = '';
+            $result['description_parser'] = 'zerro size page AI';
+            $result['result'] = '';
 
         } elseif (preg_match('/invalid request/i', $this->content)) {
             $result['status'] = 'parsered_notext';
             $result['error'] = 1;
-            $result['error_descr'] = 'Message: invalid request in AI';
-            $result['content'] = '';
+            $result['description_parser'] = 'Message: invalid request in AI';
+            $result['result'] = '';
 
         } else if (preg_match('/t have that page archived/i', $this->content)) {
             $result['status'] = 'parsered_notext';
             $result['error'] = 1;
-            $result['error_descr'] = 'AI NO this page archived';
-            $result['content'] = '';
+            $result['description_parser'] = 'AI NO this page archived';
+            $result['result'] = '';
 
         } else if (preg_match('/The machine that serves this file is down/i', $this->content)) {
             $result['status'] = 'temporary_error';
             $result['error'] = 1;
-            $result['error_descr'] = 'Message AI: that serves this file is down Temporary ERROR';
-            $result['content'] = '';
+            $result['description_parser'] = 'Message AI: that serves this file is down Temporary ERROR';
+            $result['result'] = '';
         }
 
 
-        if (isset($result['error'])) return $result;
-
-        // продолжим
+        // основное мсясо
+        // основное мсясо
+        // основное мсясо
 
         $ae = new PhpBoilerPipe\ArticleExtractor();
         $out = $ae->getContent($this->content);
-        preg_match_all("/(.*)\\n/", $out, $d);
+
+        $out = str_replace(" \r\n", "\r\n", $out);
+//        $out = str_replace("\r\n\r\n\r\n", "\r\n\r\n",$out);
+//        $out = str_replace("\r\r\r", "\r\r",$out);
+//        $out = str_replace("\n\n\n", "\n\n",$out);
+        //
+        if (!trim($out)) {
+            $result['status'] = 'parsered_notext';
+            $result['error'] = 1;
+            $result['description_parser'] = 'zerro size page AI';
+            $result['result'] = '';
+            return $result;
+        }
+
+        $result['result'] = $out;
+
+        if (isset($result['error'])) {
+            echo "Выход по косвенным признакам это ошибка";
+            return $result;
+        }
+
+        // продолжим
+
+        if (!preg_match_all("/(.*)\\n/", $out, $d)) {
+            $result['status'] = 'parsered_notext';
+            $result['error'] = 1;
+            $result['description_parser'] = 'Не вижу абазацы';
+            return $result; //!
+        }
+
 
         $lengths = array_map('strlen', $d[0]);
         $lengths = array_unique($lengths);
@@ -81,11 +131,18 @@ class HtmlParser
         if (sizeof($lengths) < 5) $lengths = array_merge($lengths, $lengths);
         if (sizeof($lengths) < 5) $lengths = array_merge($lengths, $lengths);
 
-        //Общий абстрактный класс SmoothCurve
-        include_once('../vendor/novikov/bizie/SmoothCurve.class.php');
-        include_once('../vendor/novikov/bizie/BezierCurve.class.php');
 
-        $curve = new \BezierCurve();
+        // случайно заметил что индексы бывают спропусками и не полнвеы тут
+        $lengths = array_values($lengths);
+        // не более 170
+        $lengths = array_slice($lengths, 0, 169);
+
+
+        //Общий абстрактный класс SmoothCurve
+//        include_once('../vendor/nofikoff/bizie/SmoothCurve.class.php');
+//        include_once('../vendor/nofikoff/bizie/BezierCurve.class.php');
+
+        $curve = new BezierCurve();
         //Передаем ему координаты, шаг просчета = 1
         $curve->setCoords($lengths, 1);
         $curveCoords = $curve->process();
@@ -98,32 +155,181 @@ class HtmlParser
         //        echo "Максимальный абзац длина: " . max($lengths) . "\n";
         //        echo "Средний абзац длина: " . round($bezie_middle) . "\n";
         //        echo $out . "\n";
-
-        if (strlen($out) < 1500) {
+        if (mb_strlen($out) < 1500) {
             $result['status'] = 'parsered_notext';
             $result['error'] = 1;
-            $result['content'] = '';
+            $result['description_parser'] = 'Меньше 1500';
             $result['max_paragraf'] = max($lengths);
             $result['mdl_paragraf'] = round($bezie_middle);
 
             // если средний по бие абзац меньше 200 знаков это мусорная страница
         } else if (round($bezie_middle) < 200) {
             $result['status'] = 'parsered_notext';
+            $result['description_parser'] = 'Это жидкий текст средний абзац меньше 200';
             $result['error'] = 1;
-            $result['content'] = '';
             $result['max_paragraf'] = max($lengths);
             $result['mdl_paragraf'] = round($bezie_middle);
 
             // это хорошая статья
         } else {
-            $result['status'] = 'parsered_success';
-            $result['error'] = 0;
-            $result['content'] = $out;
+
+
+            $result['lang'] = $this->detectTextLanguage($out);
+
+
+            // языки офдтруем только русский и ангилсуйи
+            if ($result['lang'] == 'not detected') {
+                $result['status'] = 'parsered_notext';
+                $result['error'] = 1;
+
+            } else {
+                $result['status'] = 'parsered_success';
+                $result['error'] = 0;
+            }
+
+
+            $result['result'] = $out;
             $result['max_paragraf'] = max($lengths);
             $result['mdl_paragraf'] = round($bezie_middle);
+
+
+            $result['title'] = $this->get_title();
+            $result['description_parser'] = $this->get_description();
+            $result['h1'] = $this->get_h1();
         }
 
+
         return $result;
+    }
+
+
+    public function get_title()
+    {
+        $out = '';
+        if (preg_match('~<title>([^<]+)</title>~ui', $this->content, $d)) {
+            $out = $d[1];
+        }
+        return $out;
+    }
+
+    public function get_description()
+    {
+        $out = '';
+        if (preg_match('~"description"\s+content="([^"]+)"~uis', $this->content, $d)) {
+            $out = $d[1];
+        }
+        return $out;
+
+    }
+
+    public function get_h1()
+    {
+        $out = '';
+        if (preg_match('~<h1[^>]*>([^<]+)</h1>~uis', $this->content, $d)) {
+            $out = $d[1];
+        }
+        return $out;
+
+    }
+
+    static function detectTextLanguage($text, $default = 'not detected')
+    {
+        $supported_languages = array(
+            'en',
+//            'de', в этом проекте немецкий НЕ нужен!!!!
+            'ru',
+        );
+        // German word list
+        // from http://wortschatz.uni-leipzig.de/Papers/top100de.txt
+        $wordList['de'] = array('der', 'die', 'und', 'in', 'den', 'von',
+            'zu', 'das', 'mit', 'sich', 'des', 'auf', 'für', 'ist', 'im',
+            'dem', 'nicht', 'ein', 'Die', 'eine');
+
+        // English word list
+        // from http://en.wikipedia.org/wiki/Most_common_words_in_English
+        $wordList['en'] = array('the', 'be', 'to', 'of', ' and ', 'a', 'in',
+            'that', 'have', 'I', 'it', 'for', 'not', 'on', 'with', 'he',
+            'as', 'you', 'do', 'at');
+
+        // ru.wiktionary.org/wiki/Приложение:Список_частотности_по_НКРЯ
+        $wordList['ru'] = array(
+            'и',
+            'в',
+            'не',
+            'на',
+            'я',
+            'быть',
+            'он',
+            'с',
+            'что',
+            'а',
+            'по',
+            'это',
+            'она',
+            'этот',
+            'к',
+            'но',
+            'они',
+            'мы',
+            'как',
+            'из');
+
+        // clean out the input string - note we don't have any non - ASCII
+        // characters in the word lists... change this if it is not the
+        // case in your language wordlists!
+        $text = preg_replace("/[^a-zа-я]/ui", ' ', $text);
+        // count the occurrences of the most frequent words
+        foreach ($supported_languages as $language) {
+            $counter[$language] = 0;
+        }
+        for ($i = 0; $i < 20; $i++) {
+            foreach ($supported_languages as $language) {
+                $counter[$language] = $counter[$language] +
+                    // I believe this is way faster than fancy RegEx solutions
+                    substr_count($text, ' ' . $wordList[$language][$i] . ' ');;
+            }
+        }
+
+        //print_r($counter);
+
+
+        // get max counter value
+        // from http://stackoverflow.com/a/1461363
+        $max = max($counter);
+        $maxs = array_keys($counter, $max);
+        // if there are two winners - fall back to default!
+        if (count($maxs) == 1) {
+            $winner = $maxs[0];
+            $second = 0;
+            // get runner-up (second place)
+            foreach ($supported_languages as $language) {
+                if ($language <> $winner) {
+                    if ($counter[$language] > $second) {
+                        $second = $counter[$language];
+                    }
+                }
+            }
+            // apply arbitrary threshold of 10%
+            if (($second / $max) < 0.1) {
+                return $winner;
+            }
+        }
+
+        print_r($counter);
+
+        return $default;
+    }
+
+    static function detect_encoding($string)
+    {
+        static $list = array('utf-8', 'windows-1251');
+
+        foreach ($list as $item) {
+            $sample = mb_convert_encoding($string, $item, $item);
+            if (md5($sample) == md5($string))
+                return $item;
+        }
+        return null;
     }
 
 }
--- UniqueTestTxt.php
+++ UniqueTestTxt.php
@@ -4,43 +4,154 @@
  */
 
 
+/**
+ * этот алгоритм взять тут http://www.c-laboratory.ru/seo/unik/
+ *
+ *
+ * то есть у нас должны оставаться только те котоые оригинальные или те которые с удаленного сайта но еще в
+ * кэше и нигде больше нет....если например статья в кэше и еще где то. то ее нет смысла хранить, а если она
+ * только в кэше а нигде больше нет то ее просто в отстойник и ждать пока выйдет из кэша и как выйдет,
+ * мгновенно использовать ее(но вообще то ее для себя уже можно использовать, а как кэш пропадет то она
+ * станет сразу оригинальной)
+ *
+ *
+ * да, это основа всей системы
+ * добавлю, что при удалении НЕ укниального текста из нашей БД
+ * можем оставлять адрес истчника себе на память, чтобы снова не проскнаировать его по ошибке
+ *
+ *
+ * [23:06:25] Maxim  Rudenko: но принцып у нас такой, получили текст, проверили его по кключевым фразам статей конкурента, если нашли, то сразу в оригинальные тексты, если не нашли то проверяем на оригинальность, те что не оригинальны в одно место, те сто оригинальны в другое и их там на кэш проверяем и сортируем оригинальные с кэшем  и оригинальные без кэша, как я выше написал  и все
+ * [23:07:55] Ruslan Novikov: это типа для экономии ресурсов и времени на проверку уникальности
+ * ок- попробуем
+ *
+ *
+ * Храним в БД
+ * Есть замеры. Производительность начинает заметно сильнее просидать после того как фаил переваливает около 5 мб.
+ * Замеры были на оракле и mssql про mysql мне кажется тоже можно нарыть. Исследование проводилось каким-то институтом американским. Резюме было такое, что хранить до 5 мб файлы можно со свистом в БД, после 5 лучше в файловой системе, именно по этому в оракле и mssql есть на этот счет специальный функционал (filestreaming и т.д.) это когда база сама следит за целостностью файлов, которые хранятся в файловой системе.
+ *
+ *
+ * Указываем сайт из архива и есть три опции: 1 слить просто все что есть один к одному HTML,  то есть точно так как у них с url  2 слить только тексты с заглавными h1 h2 h3, метатегами с разбивкой по url как у них есть 3 тоже самое как во втором, но только в xml образец который я дам.  Нигде для этого проверять ни на кэш ни на оригинальной не надо.! Вот такое думаю вообще не сложно сделать
+ *
+ */
+
+
 namespace nofikoff\parsermachine;
 
-use DotPack\PhpBoilerPipe;
+use app\controllers\SystemMessagesLogController;
+use nofikoff\yashagosha\GoogleMachine;
+
+
 //use Yii;
 
 class UniqueTestTxt
 {
 
+    // минимальная длина предложения что выираем из статьи для анализа
+    public $min_length_str = 50; // в оригианле = 50
+    public $number_need_str = 5; // в оригинале = 10
+    //полноценная статья с предложеиями на входе
+    public $article_str = '';
+    public $debug = false;
 
-public    function go_foundpages( $strOnePhrase) {
-        // функция ищет на гугле фразу и возвращает кол-во найденных страниц
-        // http://www.google.ru/search?q=
-        $strReq = trim($strOnePhrase);
-        $strReq = '"'.$strReq.'"';
-        $strReq = urlencode($strReq);
 
-        $strGoAnswer = file_get_contents('http://www.google.ru/search?filter=0&q=' . $strReq);
+    public $google;
 
-        if( !strlen($strGoAnswer)) {
-            print("<br><br>гугль забанил IP! ждем 5 минут, пока разбанит");
-            exit;
-        }
+    function __construct()
+    {
+        $this->google = new GoogleMachine();
+        $this->google->exactly = true;
+        $this->google->debug = $this->debug;
 
-        //print( RemScripts($strGoAnswer));
-        $strGoAnswer = preg_replace ("'<head>.*?</head>'si", "", $strGoAnswer); // целиком удаляем хедер
-        //print( $strGoAnswer);
 
-        //Результаты <b>1</b> - <b>1</b> из <b>1</b> для <b>
-        //Результаты <b>1</b> - <b>10</b> из примерно <b>66</b> для
-
-        if( preg_match( "|Результаты <b>\d+</b> - <b>\d+</b> из .{0,9}<b>(\d+)</b> для <b>|si", $strGoAnswer, $arrTmp)) {
-            $intAnswer = 0 + $arrTmp[1];
-        } else {
-            $intAnswer = 0;
-        }
-        //print( $strOnePhrase ." - " . $intAnswer . "<br>");
-        return $intAnswer;
     }
+
+    public function result()
+    {
+        $index = 0; // количество найденных дублей предложений
+        $total = 0; // количество учтенных дублей предложений
+
+        // получаем массив чищенный прдложений от большего к меньшему
+        $list_str = $this->get_array_n_max_str_from_article($this->article_str);
+
+        $result['error'] = false;
+        $result['desc'] = '';
+
+
+        foreach ($list_str as $str) {
+            $str = $this->get_str_with_n_max_words($str);
+            $out_page = $this->google->get_page($str);
+            if ($out_page['error']) {
+                $result['error'] = true;
+                $result['desc'] = $out_page['desc'];
+                return $result;
+            }
+
+
+            $num_result = $this->google->parse_number_finded_results($out_page['result']);
+            if ($num_result['error']) {
+                $result['error'] = true;
+                $result['desc'] = $num_result['desc'];
+                return $result;
+            }
+
+
+            $result['ResultPhrases'][] = $str;
+            $result['ResultFound'][] = $num_result['result'];
+            if ($num_result > 0) {
+                $index = $index + $num_result['result'];
+                $total++;
+            }
+        }
+
+        if ($total > 0) {
+            $result['level_uniq'] = round((1 / ($index / $total)), 2);
+            $result['desc'] = 'Уникальность по Гуглу';
+        } else {
+            $result['level_uniq'] = "-1";
+            $result['desc'] = "Абсолютно уникальный текст, не встречается в Гугле";
+        }
+
+        return $result;
+    }
+
+
+
+    // подготавливает строку запроса, убирает знаки препинания,
+    // лишние пробелы, обрезает до максимально допустимйо длины
+    public function get_str_with_n_max_words($strReq)
+    {
+        // обрезаем до 30 слов в запросе
+        $a = explode(' ', $strReq);
+        $a = array_slice($a, 0, $this->google->query_max_length);
+        $strReq = implode(' ', $a);
+        return $strReq;
+    }
+
+    // подготавливает строку запроса, убирает знаки препинания,
+    // лишние пробелы, обрезает до максимально допустимйо длины
+    public function get_array_n_max_str_from_article($strReq)
+    {
+        // чистим статью от спецзнаков и пр
+        $strReq = preg_replace('/[^а-яa-z0-9\.]+/ui', ' ', $strReq);
+        $strReq = preg_replace('/[\s]+/', ' ', $strReq);
+        // разбиваем напредложения
+        $a = explode('.', $strReq);
+        // тримаем полученный массив предложений
+        $a = array_map("trim", $a);
+        // сортируем от больших к меньгим
+        usort($a, function ($a, $b) {
+            return mb_strlen($b) - mb_strlen($a);
+        });
+
+        $total_str = 0;
+        foreach ($a as $str) {
+            if (mb_strlen($str) < $this->min_length_str) break;
+            $total_str++;
+            if ($total_str > ($this->number_need_str - 1)) break;
+        }
+        // возвращаем обрещанный до нужного числа предложений массив
+        return array_slice($a, 0, $total_str);
+    }
+
 
 }
